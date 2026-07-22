a1a05d12dee4d4a3d3454f69185966091f2480f4
Zyqorv <zekovnickita@gmail.com>
2026-07-22 00:31:00 -0400

    fixed config for gamemessage

--- app/src/game/gameMessage.php
+++ app/src/game/gameMessage.php
@@ -15,7 +15,7 @@ $request = [
 try {
     require_once __DIR__ . "/../RabbitMQLib.inc";
 
-    $client = new rabbitMQClient(__DIR__ . "/gameRabbitMQ.ini");
+    $client = new rabbitMQClient(__DIR__ . "/../../config/gameRabbitMQ.ini");
     $response = $client->send_request($request);
 
     echo "Client received response:" . PHP_EOL;
